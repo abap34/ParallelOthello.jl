b165526c914eda05a3ee535b905e10b6dfe18454
abap34 <uchi_ymgc@her0.jp>
2023-07-15 23:43:59 +0900

    refactor

--- src/solvers/minmax.jl
+++ src/solvers/minmax.jl
@@ -6,10 +6,9 @@ function choice(solver::MinMax, board1::UInt64, board2::UInt64, legals::UInt64):
     max_depth = solver.max_depth
     cand = LegalCand(legals)
     scores = zeros(Int, length(cand))
-    hands = zeros(UInt64, length(cand))
+    hands = collect(LegalCand(legals))
     for (i, legal) in enumerate(cand)
-        scores[i] = -minmax(1, put(board1, board2, legal)..., max_depth)
-        hands[i] = legal
+        scores[i] = minmax(1, put(board1, board2, legal, "black")..., max_depth)
     end
     res = choice_maximum(scores, hands)
     if res == 0x0
@@ -20,48 +19,36 @@ function choice(solver::MinMax, board1::UInt64, board2::UInt64, legals::UInt64):
 end
 
 function minmax(depth::Int, board1::UInt64, board2::UInt64, max_depth::Int)
-    turn = depth % 2
-
     if depth == max_depth
-        if turn == 1
-            return count_ones(board1) - count_ones(board2)
-        else
-            return count_ones(board2) - count_ones(board1)
-        end
+        return count_ones(board1) - count_ones(board2)
     end
 
-    score = 100000000000
 
+    if isfinish(board1, board2)[1]
+        return count_ones(board1) - count_ones(board2)
+    end
 
-    if turn == 1
-        if isfinish(board1, board2)[1]
-            return count_ones(board1) - count_ones(board2)
-        end
+    if depth % 2 == 0
         _legals = legal(board1, board2)
-        # pass
-        if _legals == 0x0
-            score = -minmax(depth + 1, board1, board2, max_depth)
-        else
-            cand = LegalCand(_legals)
-            for legal in cand
-                score = min(score, -minmax(depth + 1, put(board1, board2, legal)..., max_depth))
-            end
-            return score
-        end
     else
-        if isfinish(board1, board2)[1]
-            return count_ones(board2) - count_ones(board1)
-        end
         _legals = legal(board2, board1)
-        # pass
-        if _legals == 0x0
-            score = -minmax(depth + 1, board1, board2, max_depth)
-        else
-            cand = LegalCand(_legals)
+    end
+
+    if _legals == 0x0
+        score = -minmax(depth + 1, board1, board2, max_depth)
+    else
+        cand = LegalCand(_legals)
+        if depth % 2 == 1
+            score = 100000000
             for legal in cand
-                score = min(score, -minmax(depth + 1, put(board2, board1, legal)..., max_depth))
+                score = min(score, minmax(depth + 1, put(board1, board2, legal, "white")..., max_depth))
             end
-            return score
+        else
+            score = -10000000000
+            for legal in cand
+                score = max(score, minmax(depth + 1, put(board1, board2, legal, "black")..., max_depth))
+            end
         end
     end
+    return score
 end
