d1d14cffcc39aeca0cc1d90b530e05f225260740
abap34 <uchi_ymgc@her0.jp>
2023-07-16 01:29:23 +0900

    clean up

--- src/solvers/parrallelminmax.jl
+++ src/solvers/parrallelminmax.jl
@@ -1,23 +1,18 @@
-using Base.Threads
+using Base: Threads
 
 struct ParallelMinMax <: AbstractSolver
     max_depth::Int
-    num_threads::Int
 end
-
-
 
 function choice(solver::ParallelMinMax, board1::UInt64, board2::UInt64, legals::UInt64)::UInt64
     max_depth = solver.max_depth
     cand = LegalCand(legals)
-    n = length(cand)
-    scores = zeros(Int, n)
-    hands = zeros(UInt64, n)
+    hands = collect(LegalCand(legals))
+    n = length(hands)
     tasks = map(1:n) do i
-        @spawn -paralellminmax(1, put(board1, board2, cand[i])..., max_depth)
+        Threads.@spawn paralellminmax(1, put(board1, board2, cand[i], "black")..., max_depth)
     end
     scores = (fetch(task)::Int for task in tasks)
-    hands = collect(cand)
     res = choice_maximum(scores, hands)
     if res == 0x0
         throw(DomainError("zero choice. \n hands:$hands, \n scores:$scores"))
@@ -26,49 +21,37 @@ function choice(solver::ParallelMinMax, board1::UInt64, board2::UInt64, legals::
     end
 end
 
-
-function build_score_function(depth, board1, board2, max_depth)
-    return (legal) -> (paralellminmax(depth + 1, put(board1, board2, legal)..., max_depth))
-end
-
-function paralellminmax(depth::Int, board1::UInt64, board2::UInt64, max_depth::Int)::Int
+function paralellminmax(depth::Int, board1::UInt64, board2::UInt64, max_depth::Int)
     if depth == max_depth
         return count_ones(board1) - count_ones(board2)
     end
 
-    turn = depth % 2
-
-
-    score = 100000000000
 
     if isfinish(board1, board2)[1]
-        return count_ones(board2) - count_ones(board1)
+        return count_ones(board1) - count_ones(board2)
     end
-    if turn == 1
+
+    if depth % 2 == 0
         _legals = legal(board1, board2)
-        # pass
-        if _legals == 0x0
-            return -paralellminmax(depth + 1, board1, board2, max_depth)
-        else
-            cand = LegalCand(_legals)
-            if depth == 1
-                for legal in cand
-                    score = min(score, -minmax(depth + 1, put(board1, board2, legal)..., max_depth))
-                end
-            end
-            return score
-        end
     else
         _legals = legal(board2, board1)
-        # pass
-        if _legals == 0x0
-            return -paralellminmax(depth + 1, board1, board2, max_depth)
-        else
-            cand = LegalCand(_legals)
+    end
+
+    if _legals == 0x0
+        score = paralellminmax(depth + 1, board1, board2, max_depth)
+    else
+        cand = LegalCand(_legals)
+        if depth % 2 == 1
+            score = 100000000
             for legal in cand
-                score = min(score, -minmax(depth + 1, put(board2, board1, legal)..., max_depth))
+                score = min(score, paralellminmax(depth + 1, put(board1, board2, legal, "white")..., max_depth))
             end
-            return score
+        else
+            score = -10000000000
+            for legal in cand
+                score = max(score, paralellminmax(depth + 1, put(board1, board2, legal, "black")..., max_depth))
+            end
         end
     end
+    return score
 end
